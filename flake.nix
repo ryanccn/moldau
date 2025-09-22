f0c6f94073928e41e7b836105eac50f08a12568c
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2025-09-22 23:37:02 +0800

    chore: housekeeping

--- flake.nix
+++ flake.nix
@@ -5,144 +5,24 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    ferrix.url = "github:ryanccn/ferrix";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      inherit (nixpkgs) lib;
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+    { ferrix, ... }@inputs:
+    ferrix.lib.mkFlake inputs {
+      root = ./.;
+      completions.enable = true;
 
-      forAllSystems = lib.genAttrs systems;
-      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      checks = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-
-          mkFlakeCheck =
-            args:
-            pkgs.stdenv.mkDerivation (
-              {
-                name = "check-${args.name}";
-                src = self.packages.${system}.moldau.src;
-
-                buildPhase = ''
-                  ${args.command}
-                  touch "$out"
-                '';
-
-                doCheck = false;
-                dontInstall = true;
-                dontFixup = true;
-              }
-              // (removeAttrs args [
-                "name"
-                "command"
-              ])
-            );
-        in
-        {
-          nixfmt = mkFlakeCheck {
-            name = "nixfmt";
-            command = "find . -name '*.nix' -exec nixfmt --check {} +";
-
-            src = self;
-            nativeBuildInputs = with pkgs; [ nixfmt-rfc-style ];
-          };
-
-          rustfmt = mkFlakeCheck {
-            name = "rustfmt";
-            command = "cargo fmt --check";
-
-            nativeBuildInputs = with pkgs; [
-              cargo
-              rustfmt
-            ];
-          };
-
-          clippy = mkFlakeCheck {
-            name = "clippy";
-            command = ''
-              cargo clippy --all-features --all-targets --tests \
-                --offline --message-format=json \
-                | clippy-sarif | tee $out | sarif-fmt
-            '';
-
-            inherit (self.packages.${system}.moldau) cargoDeps;
-            nativeBuildInputs = with pkgs; [
-              rustPlatform.cargoSetupHook
-              cargo
-              rustc
-              clippy
-              clippy-sarif
-              sarif-fmt
-            ];
-          };
-
-          reuse = mkFlakeCheck {
-            name = "reuse";
-            command = "reuse lint";
-
-            src = self;
-            nativeBuildInputs = with pkgs; [ reuse ];
-          };
-        }
-      );
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustfmt
-              clippy
-              rust-analyzer
-            ];
-
-            inputsFrom = [ self.packages.${system}.moldau ];
-
-            env = {
-              RUST_BACKTRACE = 1;
-              RUST_SRC_PATH = toString pkgs.rustPlatform.rustLibSrc;
-            };
-          };
-        }
-      );
-
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-          packages = self.overlays.default null pkgs;
-        in
-        {
-          inherit (packages) moldau;
-          default = packages.moldau;
-        }
-      );
-
-      legacyPackages = forAllSystems (
-        system: nixpkgsFor.${system}.callPackage ./nix/static.nix { inherit self; }
-      );
-
-      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
-
-      overlays.default = _: prev: {
-        moldau = prev.callPackage ./nix/package.nix { inherit self; };
+      extraPostInstall = {
+        includeShims = {
+          default = false;
+          value = ''
+            for shim in npm npx yarn yarnpkg pnpm pnpx; do
+              ln -s "$out/bin/moldau" "$out/bin/$shim"
+            done
+          '';
+        };
       };
     };
 }
